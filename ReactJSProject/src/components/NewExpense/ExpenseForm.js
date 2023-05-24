01501865d81918aa63b14c749b046f66bcf54c4c
Siddhu9966 <134256391+Siddhu9966@users.noreply.github.com>
2023-05-24 11:55:29 +0000

    commit

--- ReactJSProject/src/components/NewExpense/ExpenseForm.js
+++ ReactJSProject/src/components/NewExpense/ExpenseForm.js
@@ -1,19 +1,70 @@
+import React, {useState} from 'react';
 import './ExpenseForm.css';
-function ExpenseForm(){
+function ExpenseForm(props){
+    const [enteredTitle,setEnteredTitle] = useState('');
+    const [enteredAmount,setEnteredAmount] = useState('')
+    const [enteredDate, setEnteredDate] = useState('');
+    //const [userInput, setUserInput]=useState({
+    //    enteredTitle: '',
+    //   enteredAmount: '',
+    //    enteredDate: ''
+    //});
+    function titleChangeHandler(event){
+        setEnteredTitle(event.target.value);
+        //One way
+      //  setUserInput({
+       //     ...userInput,
+        //    enteredTitle: event.target.value,
+        //})
+        //Second way(Safest Way)
+        //setUserInput((prevState)=>{
+        //    return{...prevState,enteredTitle:event.target.value};
+        //});
+
+    };
+    function amountChangeHandler(event){
+      setEnteredAmount(event.target.value);
+      //  setUserInput({
+       //     ...userInput,
+       //     enteredAmount: event.target.value,
+       // })
+    }
+
+    function dateChangeHandler(event){
+      setEnteredDate(event.target.value);
+        //setUserInput({
+       //     ...userInput,
+       //     enteredDate: event.target.value,
+       // })
+        
+    }
+    function submitHandler(event){
+        event.preventDefault(); //it is used to not reload the page(not sending the request to the server) when the form is submitted. We are using it because if there isn't any logic in the function.
+        
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+       props.onSaveExpenseData(expenseData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
+    };
     return(
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="new-expense__controls"> 
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text"/>
+                    <input type="text" value={enteredTitle} onChange={titleChangeHandler}/>
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" /> 
+                    <input type="number" min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler}/> 
                 </div>
                 <div className="new-expense__control"> 
                     <label>Date</label>
-                    <input type='date' min="2019-01-01" max="2022-12-31" />
+                    <input type='date' min="2019-01-01" max="2022-12-31" value={enteredDate} onChange={dateChangeHandler}/>
                 </div>
             </div>
             <div className="new-expense__actions">
